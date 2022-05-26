cb682cd7bbf267858bb2ded0479015b7051068c1
“Akshu864” <bhardwahjakshay14@gmail.com>
2022-05-26 23:42:10 +0530

    okkey

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -311,9 +311,7 @@ const getProductById = async  function(req,res) {
 
         const productId = req.params.productId
 
-        if (productId === "") {
-            return res.status(400).send({ status: false, message: "plzz provide the valid product size" })
-        }
+        
 
         if (!isValidObjectId(productId)) {
             return res.status(400).send({ status: false, message: "product id is invalid" })
